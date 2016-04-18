f4be8543ee76d9e466ee9a46d8ed21b45102fc8f
kolya-t <toporkov.kolya96@gmail.com>
2016-04-18 03:17:07 +0300

    Кнопка-Начало-Росомаха

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,50 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QRect>
+#include <QDesktopWidget>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
-    ui(new Ui::MainWindow)
+    ui(new Ui::MainWindow),
+    count(0), step(1.1)
 {
     ui->setupUi(this);
+
+    // центрирование кнопки
+    QRect widgetGeometry = ui->pushButton->geometry();
+    QPoint center((geometry().width() - widgetGeometry.width()) / 2,
+                  (geometry().height() - widgetGeometry.height()) / 2);
+    ui->pushButton->move(center);
+
+    // центрирование счетчика
+    widgetGeometry = ui->label->geometry();
+    center = QPoint((geometry().width() - widgetGeometry.width()) / 2,
+                    (ui->pushButton->geometry().y() - widgetGeometry.height()) / 2);
+    ui->label->move(center);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+//void MainWindow::centerWidgetOnParent(QWidget* widget, const QWidget* parent) {
+//    QRect parentGeometry = parent->geometry();
+//    QRect widgetGeometry = ui->pushButton->geometry();
+//    QPoint center((parentGeometry.width() - widgetGeometry.width()) / 2,
+//                  (parentGeometry.height() - widgetGeometry.height()) / 2);
+//    widget->move(center);
+//}
+
+//void MainWindow::centerWidgetOnRect(QWidget* widget, const QRect& rect) {
+//    QRect widgetGeometry = widget->geometry();
+//    QPoint center((rect->width() - widget->width()) / 2,
+//                  (rect->height() - widget->height()) / 2);
+//    widget->move(center);
+//}
+
+void MainWindow::on_pushButton_clicked() {
+    count += step;
+    ui->label->setText(QString::number(count));
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QtSvg/QSvgWidget>
+#include <QtSvg/QSvgRenderer>
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +17,16 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+//    void centerWidgetOnParent(QWidget* widget, const QWidget* parent);
+//    void centerWidgetOnRect(QWidget* widget, const QRect& rect);
+private slots:
+    void on_pushButton_clicked();
+
 private:
     Ui::MainWindow *ui;
+
+    double count;
+    double step;
 };
 
 #endif // MAINWINDOW_H
