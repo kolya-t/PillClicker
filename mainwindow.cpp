8442bb4eff705f0d8ad5e65ee71ea13b346e658b
kolya-t <toporkov.kolya96@gmail.com>
2016-04-18 17:25:53 +0300

    1) Изменен алгоритм центрирования элементов на экране. 2) Добавлен множитель таблеток при множественном нажатии кнопки, он пропадает, если кнопка не нажималась 2 секунды

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,58 +7,89 @@
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow),
-    count(0), step(1.1)
+    _timer(new QTimer(this)),
+    _pillsCount(0), _step(1)
 {
+    connect (_timer, SIGNAL(timeout()), this, SLOT(nullMultiplier()));
+    /*----------------УСТАНОВКА ИНТЕРФЕЙСА-НАЧАЛО-------------------*/
     ui->setupUi(this);
+    nullMultiplier();
+    /* размеры приложения */
+    // на десктопе
+    QRect screenGeometry = geometry();
+    // на андроиде
+//    QRect screenGeometry = qApp->desktop()->availableGeometry(ui->pushButton);
 
     // установка размеров кнопки
-    pillSize = geometry().width() / 2;
-    ui->pushButton->resize(pillSize, pillSize);
-    ui->pushButton->setIconSize(QSize(pillSize, pillSize));
+    _pillSize = screenGeometry.width() / 2;
+    ui->pillButton->resize(_pillSize, _pillSize);
+    ui->pillButton->setIconSize(QSize(_pillSize, _pillSize));
 
     // центрирование кнопки
-    QPoint center((geometry().width() - pillSize) / 2,
-                  (geometry().height() - pillSize) / 2);
-    ui->pushButton->move(center);
+    QPoint center(centerXBetweenAB(_pillSize, 0, screenGeometry.width()),
+                  centerXBetweenAB(_pillSize, 0, screenGeometry.height()));
+    ui->pillButton->move(center);
 
     // центрирование счетчика
-    QRect widgetGeometry = ui->label->geometry();
-    center = QPoint((geometry().width() - widgetGeometry.width()) / 2,
-                    (ui->pushButton->geometry().y() - widgetGeometry.height()) / 2);
-    ui->label->move(center);
+    QRect widgetGeometry = ui->counterLabel->geometry();
+    center = QPoint(centerXBetweenAB(widgetGeometry.width(), 0, screenGeometry.width()),
+                    centerXBetweenAB(widgetGeometry.height(), 0, ui->pillButton->y()));
+    ui->counterLabel->move(center);
+
+    // центрирование шага
+    widgetGeometry = ui->stepLabel->geometry();
+    center = QPoint(centerXBetweenAB(widgetGeometry.width(), 0, screenGeometry.width()),
+                   centerXBetweenAB(widgetGeometry.height(), _pillSize + ui->pillButton->y(), screenGeometry.height()));
+    ui->stepLabel->move(center);
+    /*----------------УСТАНОВКА ИНТЕРФЕЙСА-КОНЕЦ--------------------*/
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
     delete ui;
 }
 
-//void MainWindow::centerWidgetOnParent(QWidget* widget, const QWidget* parent) {
-//    QRect parentGeometry = parent->geometry();
-//    QRect widgetGeometry = ui->pushButton->geometry();
-//    QPoint center((parentGeometry.width() - widgetGeometry.width()) / 2,
-//                  (parentGeometry.height() - widgetGeometry.height()) / 2);
-//    widget->move(center);
-//}
-
-//void MainWindow::centerWidgetOnRect(QWidget* widget, const QRect& rect) {
-//    QRect widgetGeometry = widget->geometry();
-//    QPoint center((rect->width() - widget->width()) / 2,
-//                  (rect->height() - widget->height()) / 2);
-//    widget->move(center);
-//}
-
-void MainWindow::on_pushButton_clicked() {
-    count += step;
-    ui->label->setText(QString::number(count));
+/* Возвращает такую координату, чтобы отрезок,
+ * длиной x оказался ровно по центру между begin и end */
+int MainWindow::centerXBetweenAB(int x, int begin, int end) {
+    return begin + (end - begin - x) / 2;
 }
 
+void MainWindow::on_pillButton_clicked() {
+    // получение таблеточек
+    _pillsCount += _step * _multiplier;
+    // вывод информации на экран
+    ui->counterLabel->setText(QString::number(_pillsCount));
+    ui->stepLabel->setText(tr("+") + QString::number(_step * _multiplier));
 
-void MainWindow::on_pushButton_pressed() {
-    ui->pushButton->setIconSize(QSize(pillSize / 2, pillSize));
+    /* временный множитель, пропадающий через 2 секунды
+     * после прекращения кликания. увеличивает свое значение
+     * каждые 5 кликов на 10 процентов */
+    ++_clicksCount; // был произведен клик
+    // если 2 секунды еще не прошло
+    if (_timer->isActive()) {
+        if (_clicksCount == CLICKS_TO_INC_MULTIPLIER) {
+            _multiplier += _step * MULTIPLIER_INCREMENT;
+            _clicksCount = 0;
+        }
+    }
+    // если прошло 2 секунды после последнего клика
+    else {
+        nullMultiplier();
+    }
+    _timer->start(TIME_TO_NULL_MULTIPLIER);
 }
 
+void MainWindow::on_pillButton_pressed() {
+    ui->pillButton->setIconSize(QSize(_pillSize / 2, _pillSize));
+}
 
-void MainWindow::on_pushButton_released() {
-    ui->pushButton->setIconSize(QSize(pillSize, pillSize));
+void MainWindow::on_pillButton_released() {
+    ui->pillButton->setIconSize(QSize(_pillSize, _pillSize));
+}
+
+// обнуление количества кликов подряд и установка множителя = 1
+void MainWindow::nullMultiplier() {
+    _clicksCount = 0;
+    _multiplier = 1;
+    ui->stepLabel->setText("");
 }
--- mainwindow.h
+++ mainwindow.h
@@ -4,6 +4,17 @@
 #include <QMainWindow>
 #include <QtSvg/QSvgWidget>
 #include <QtSvg/QSvgRenderer>
+#include <QTimer>
+
+/* параметры множителя */
+// время в миллисекундах до обнуления временного множителя
+#define TIME_TO_NULL_MULTIPLIER     2000
+// кликов до увеличения множителя
+#define CLICKS_TO_INC_MULTIPLIER    5
+// на сколько увеличивается множитель
+// через CLICKS_TO_INC_MULTIPLIER кликов
+#define MULTIPLIER_INCREMENT        0.1
+
 
 namespace Ui {
 class MainWindow;
@@ -17,21 +28,32 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
-//    void centerWidgetOnParent(QWidget* widget, const QWidget* parent);
-//    void centerWidgetOnRect(QWidget* widget, const QRect& rect);
+    /* Возвращает такую координату, чтобы отрезок,
+     * длиной x оказался ровно по центру между begin и end */
+    static int centerXBetweenAB(int x, int begin, int end);
 private slots:
-    void on_pushButton_clicked();
+    void on_pillButton_clicked();
 
-    void on_pushButton_pressed();
+    void on_pillButton_pressed();
 
-    void on_pushButton_released();
+    void on_pillButton_released();
 
-    private:
+    void nullMultiplier();
+private:
     Ui::MainWindow *ui;
-    int pillSize;
+    // размер кнопки в пикселях
+    int _pillSize;
+    // таймер
+    QTimer* _timer;
 
-    double count;
-    double step;
+    // количество таблеток
+    double _pillsCount;
+    // количество получения таблеток за клик
+    double _step;
+    // временный множитель при множественном клике
+    double _multiplier;
+    // количество кликов по таблетке подряд
+    int    _clicksCount;
 };
 
 #endif // MAINWINDOW_H
